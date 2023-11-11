59ac066d6e99116e039879afbec72fff5c364c04
KyrylIeremieiev <34677@ma-web.nl>
2023-11-11 02:14:07 +0100

    start project, add img

---
+++ remembrance/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sky from './img/sky.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={Sky}></img>
+    </div>
+  );
+}
+
+export default App;
