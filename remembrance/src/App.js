aa8b071b9a7b738388cb2ef2eb3843e1772b7c9e
KyrylIeremieiev <34677@ma-web.nl>
2023-11-11 02:49:45 +0100

    choose final photo, add color scheme

--- remembrance/src/App.js
+++ remembrance/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Sky from './img/sky.jpg'
+import forefront from './img/white.png'
 
 function App() {
   return (
     <div className="App">
-      <img src={Sky}></img>
+      <img className="banner"src={Sky}></img>
+      <section className='welcome'></section>
+      <section className='dashboard'></section>
     </div>
   );
 }
